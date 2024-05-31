289b7c17e7fcf7eb660da4029fb4ecb202f57ffc
juyoung <nwwn1004@naver.com>
2024-06-01 02:11:47 +0900

    feat: 타입스크립트 설정 수정, backend/index.ts 수정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app: Express = express();
 const port = 5000;
 
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+app.use(express.static(path.join(__dirname, '../uploads')));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('안녕하세요!!!');
+  res.send('안녕하세요');
 });
 
 app.listen(port, () => {
