c4903f69e20d1b0af3f52ef921ff9a380e6e345f
juyoung <nwwn1004@naver.com>
2024-06-02 00:29:27 +0900

    fix:확장자 ts로 인한 에러 js로 수정

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,26 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
 
-const app: Express = express();
+const app = express();
 const port = 5000;
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 app.use(express.static(path.join(__dirname, '../uploads')));
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('안녕하세요');
+app.get('/', (req, res) => {
+  res.send('안녕하세요@@@');
 });
 
+app.post('/', (req, res) => {
+  console.log('[post]', req.body);
+  res.json(req.body);
+});
 app.listen(port, () => {
   console.log(`Backend server is running on port ${port}`);
 });
