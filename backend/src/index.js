8b799fa28a6915369f371c3f74f67a3b855eb524
juyoung <nwwn1004@naver.com>
2024-06-02 01:30:52 +0900

    feat: gitignore에 evn추가, env파일에 mognDB환경설정, index에 mongoose연결

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import monggoose from 'mongoose';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 
 const app = express();
 const port = 5000;
@@ -12,6 +15,11 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 app.use(express.static(path.join(__dirname, '../uploads')));
 app.use(cors());
 app.use(express.json());
+
+monggoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('connected to mongodb'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('안녕하세요@@@');
