51db5fbac5b9f0fb900d5cb34125599a40fc84e4
juyoung <nwwn1004@naver.com>
2024-06-03 00:56:04 +0900

    feat: 회원가입 ui

--- frontend/src/pages/SingupPage/index.tsx
+++ frontend/src/pages/SingupPage/index.tsx
@@ -1,5 +1,99 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Link } from 'react-router-dom';
+// Zod 스키마 정의
+const formSchema = z.object({
+  username: z.string().min(3, 'Username must be at least 3 characters long'),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters long')
+});
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: ''
+    }
+  });
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (
+    values: z.infer<typeof formSchema>
+  ) => {
+    console.log(values);
+  };
+
+  return (
+    <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
+      <h1 className="text-3xl font-semibold text-center">회원가입</h1>
+      <div className="mt-8 p-6 shadow-lg bg-white rounded-lg ">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="이메일" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="이름" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="비밀번호" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              회원가입
+            </Button>
+          </form>
+          <p className="mt-4 text-center text-sm text-gray-700">
+            <span>아이디가 있다면?</span>
+            <Link to="/login" className="font-medium">
+              {' '}
+              로그인
+            </Link>
+          </p>
+        </Form>
+      </div>
+    </section>
+  );
 };
 
 export default SignupPage;
