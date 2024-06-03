0dd29ad815daf40333269a03d007448e4404d7dc
juyoung <nwwn1004@naver.com>
2024-06-04 01:19:54 +0900

    feat: axios 인스턴스 header셋팅, vite env 환경변수 셋팅

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 // console.log('import.meta.env.VITE_BASE_URL: ', import.meta.env.VITE_BASE_URL);
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BASE_URL ? '' : 'http://localhost:5000'
+  baseURL: import.meta.env.VITE_BASE_URL ? '' : 'http://example.com/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default axiosInstance;
