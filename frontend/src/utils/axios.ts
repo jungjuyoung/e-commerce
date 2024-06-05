9fcca6e354fcdf31112412d9f713238c2a7d89a2
juyoung <nwwn1004@naver.com>
2024-06-06 01:22:56 +0900

    feat: axios 인스턴스 수정

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 // console.log('import.meta.env.VITE_BASE_URL: ', import.meta.env.VITE_BASE_URL);
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL ? '' : 'http://example.com/api',
+  baseURL: import.meta.env.VITE_BASE_URL
+    ? import.meta.env.VITE_BASE_URL
+    : 'http://example.com/api', // 실제 API 엔드포인트를 입력하세요
   headers: {
     'Content-Type': 'application/json'
   }
