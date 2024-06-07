ba3e55a56c03be46a21f63a16ef2ecea6f49d688
juyoung <nwwn1004@naver.com>
2024-06-08 02:14:48 +0900

    feat: backend  유저가 DB에 저장되기전에 비밀번호 암호화해서 저장

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -19,6 +20,15 @@ const userSchema = mongoose.Schema({
     default: 0
   },
   image: String
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+  }
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
