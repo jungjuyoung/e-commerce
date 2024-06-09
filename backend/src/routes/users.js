0b1701b14a45d05414182d4b866704cf99935536
juyoung <nwwn1004@naver.com>
2024-06-09 16:44:54 +0900

    feat: 로그인 라우터 생성(존재하는 유저인지 확인, 비밀번호가 올바른 것인지 확인, 유저와 비밀번호가 둘다 유효하다면 accessToken생성)

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res, next) => {
-  // DB에 전달받으 req.body를 저장
+  // DB에 전달받은 req.body를 저장
   console.log('[POST] signup req: ', req.body);
   try {
     const user = new User(req.body);
     const result = await user.save();
 
-    return res.sendStatus(200).json(result);
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/singIn', async (req, res, next) => {
+  // DB에 전달받은 req.body를 저장
+  console.log('[POST] login req: ', req.body);
+  try {
+    // 존재하는 유저인지 확인
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: '이메일가 없습니다' });
+    }
+
+    // 비밀번호가 올바른 것인지 확인
+    const isMatchedPassword = await user.comparePassword(req.body.password);
+    if (!isMatchedPassword) {
+      return res.status(400).json({ message: '비밀번호가 없습니다' });
+    }
+
+    const payload = {
+      userId: user._id.toHexString()
+    };
+
+    // Token을 생성
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '1h'
+    });
+
+    return res.status(200).json({ user, accessToken });
   } catch (error) {
     next(error);
   }
