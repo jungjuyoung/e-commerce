a85281e9e3621f389e930b3748f188d20c850342
juyoung <nwwn1004@naver.com>
2024-06-09 16:41:51 +0900

    feat: mongoDB 유저 comparePassword 스키마 생성

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -25,11 +25,20 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     const salt = await bcrypt.genSalt(10);
+    console.log('[save] salt: ', salt);
     const hash = await bcrypt.hash(this.password, salt);
+    console.log('[save] hash: ', hash);
     this.password = hash;
   }
   next();
 });
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  console.log('[comparePassword] plainPassword: ', plainPassword);
+  console.log('[comparePassword] this: ', this);
+  const match = await bcrypt.compare(plainPassword, this.password);
+  return match;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
