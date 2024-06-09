ba55a8425865b8d724747a138976bfeec2a39d0e
juyoung <nwwn1004@naver.com>
2024-06-09 16:48:12 +0900

    feat: zustand에 로그인관련 상태 추가

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -9,11 +9,17 @@ export type SignUpData = {
   image: string;
 };
 
+export type LoginData = {
+  email: string;
+  password: string;
+};
+
 type AuthStore = {
   user: SignUpData | null;
   error: string | null;
   loading: boolean;
   signUp: (data: SignUpData) => Promise<void>;
+  singIn: (data: LoginData) => Promise<void>;
 };
 
 export const useAuthStore = create<AuthStore>((set) => ({
@@ -21,7 +27,6 @@ export const useAuthStore = create<AuthStore>((set) => ({
   error: null,
   loading: false,
   signUp: async (data) => {
-    console.log('[store] useAuthStore singUp(data: ', data);
     set({ loading: true, error: null });
     try {
       const response = await axiosInstance.post(`/users/signup`, data);
@@ -32,6 +37,23 @@ export const useAuthStore = create<AuthStore>((set) => ({
       } else {
         set({ error: response.data.message, loading: false });
         toast.info('회원가입 실패!');
+      }
+    } catch (error) {
+      set({ error: error as string, loading: false });
+    }
+    set({ loading: false });
+  },
+  singIn: async (data) => {
+    set({ loading: true, error: null });
+    try {
+      const response = await axiosInstance.post(`/users/singIn`, data);
+      console.log('[login] response: ', response);
+      if (response.status >= 200 && response.status < 300) {
+        set({ user: response.data, loading: false });
+        console.log('로그인 성공! 로컬스토리지에 user 저장해야함');
+      } else {
+        set({ error: response.data.message, loading: false });
+        toast.info('로그인 실패!');
       }
     } catch (error) {
       set({ error: error as string, loading: false });
